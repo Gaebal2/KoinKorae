445f2b98df4b2385397bbd06aecd522241ded5aa
Gaebal2 <gaebal2017@gmail.com>
2026-08-12 15:09:57 +0900

    Redesign mobile home feed

--- public/sw.js
+++ public/sw.js
@@ -1,3 +1,3 @@
-const CACHE='korae-v1';
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./manifest.webmanifest','./icon.svg']))));
+const CACHE='korae-v2';
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./manifest.webmanifest','./icon-192.png','./icon-512.png']))));
 self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request).then(r=>r||caches.match('./')))));
